7e051ac111e4f77757ac4503e22c77d2879596c3
MaddieSavic <30588636+MaddieSavic@users.noreply.github.com>
2021-09-27 16:59:08 +1000

    finished condition bar chart and added to html page

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -7,3 +7,8 @@ var vg_2 = "js/purpose.vg.json";
 vegaEmbed("#purposeBar", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "js/condition.vg.json";
+vegaEmbed("#conditionBar", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
