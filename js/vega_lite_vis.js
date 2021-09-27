aa16e233202dd2e762b48f3df802df5f9e054c4f
MaddieSavic <30588636+MaddieSavic@users.noreply.github.com>
2021-09-27 15:26:20 +1000

    added stacked bar chart of purpose, also on html

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "js/choroplethmap.vg.json";
 vegaEmbed("#importMap", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "js/purpose.vg.json";
+vegaEmbed("#purposeBar", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
